7d33df4fadb20c8cd84c4a7c15e575893d9e479d
mkcnly <mkcnly@gmail.com>
2018-09-25 11:25:01 -0700

    added function as arguments (1)

--- higherOrderDrills.js
+++ higherOrderDrills.js
@@ -1,1 +1,21 @@
+'use strict';
 
+//Functions as arguments(1)
+
+function repeat(callback,number){
+    for(let i=0; i < number; i++){
+        callback();
+    }
+} 
+
+
+function hello() {
+    console.log('Hello world');
+}
+
+function goodbye() {
+    console.log('Goodbye world');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
