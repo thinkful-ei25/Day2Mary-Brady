ce157272b6f690de0f14eb885a4a84d3999c3961
Box <bradycfox@gmail.com>
2018-09-25 14:56:36 -0400

    functions as arguments (2)

--- higherOrderDrills.js
+++ higherOrderDrills.js
@@ -2,7 +2,7 @@
 
 //Functions as arguments(1)
 
-function repeat(callback,number){
+/* function repeat(callback,number){
     for(let i=0; i < number; i++){
         callback();
     }
@@ -18,4 +18,31 @@ function goodbye() {
 }
 
 repeat(hello, 5);
-repeat(goodbye, 5);
+repeat(goodbye, 5); */
+
+
+
+// Functions as Arguments (2)
+
+function filter(arr, callback) {
+  let newArray = [];
+  for(let i = 0; i < arr.length; i++){
+    if(callback(arr[i])) {
+      newArray.push(arr[i]);
+    }  
+  }
+  return newArray;
+}
+
+
+
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+
+const filteredNames = filter(myNames, function(name) {
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// 
