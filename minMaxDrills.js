510156fe534c5cd030e5693072fdc06c87f3284e
Box <bradycfox@gmail.com>
2018-09-25 12:41:32 -0400

    max function

--- minMaxDrills.js
+++ minMaxDrills.js
@@ -1,7 +1,24 @@
+'use strict';
+
 function max(numbers){
-    return largestNumber;
+  let largestNumber = numbers[0];
+  let counter = 1;
+  while (counter <= numbers.length) {
+    if (numbers[counter] > largestNumber) {
+      largestNumber = numbers[counter];
+    }
+    counter++;
+  }
+  console.log(largestNumber);
+  return largestNumber;
 }
 
-function min(numbers){
-    return smallestNumber;
-}
+// function min(numbers){
+//   return smallestNumber;
+// }
+
+
+//Tests
+let testArray = [1, 4, 5, 6, 10, 100, 4, 2];
+
+max(testArray);
